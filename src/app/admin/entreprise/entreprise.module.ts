4bd8c4da6236b560176ac883a3d415a0b55a2a73
foleyKant <krayediego@gmail.com>
2023-12-06 19:11:43 +0100

    Modif

--- src/app/admin/entreprise/entreprise.module.ts
+++ src/app/admin/entreprise/entreprise.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EntrepriseRoutingModule } from './entreprise-routing.module';
+import { CreateComponent } from './create/create.component';
+import { ViewComponent } from './view/view.component';
+import { AllComponent } from './all/all.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateComponent,
+    ViewComponent,
+    AllComponent
+  ],
   imports: [
     CommonModule,
     EntrepriseRoutingModule
