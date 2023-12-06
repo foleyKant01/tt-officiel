9aa9e4ea397823ba7d2c9058a2454285fb173a67
foleyKant <krayediego@gmail.com>
2023-12-06 12:21:11 +0100

    Modif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'super-admin', pathMatch: 'full'},
+  {path: '', redirectTo: 'auth', pathMatch: 'full'},
   {path: 'super-admin',
     loadChildren: () => import('./super-admin/super-admin.module').then((m) => m.SuperAdminModule)
   },
   {path: 'user',
     loadChildren: () => import('./user/user.module').then((m) => m.UserModule)
+  },
+  {path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule)
   },
 ];
 
