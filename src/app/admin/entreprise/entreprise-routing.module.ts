2ff5254ff01ddc6730705472a81d6434676a4846
foleyKant <krayediego@gmail.com>
2023-12-07 11:10:13 +0100

    Modif

--- src/app/admin/entreprise/entreprise-routing.module.ts
+++ src/app/admin/entreprise/entreprise-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntrepriseComponent } from './entreprise.component';
+import { CreateComponent } from './create/create.component';
+import { AllComponent } from './all/all.component';
+import { ViewComponent } from './view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:EntrepriseComponent,
+  children:[
+    {path:'', redirectTo:'create', pathMatch:'full'},
+    {path:'create', component:CreateComponent},
+    {path:'all', component:AllComponent},
+    {path:'view', component:ViewComponent}
+  ]
+},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
